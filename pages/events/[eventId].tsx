5cda244b41dbc2cd55bcee01b729c1380709b793
alfonsojim1897 <alfonsojim1897@gmail.com>
2023-05-16 13:01:12 +0200

    first commit

---
+++ pages/events/[eventId].tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SpecificEvent = () => {
+  return <div>Event Details Page</div>;
+};
+
+export default SpecificEvent;
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePage = () => {
+  return <div>Featured events</div>;
+};
+
+export default HomePage;
