5622091468ab509df68dd4807116bc0817b01b8a
alfonsojim1897 <alfonsojim1897@gmail.com>
2023-05-17 20:58:57 +0200

    Add event list comp

---
+++ components/events/EventList.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+import EventItem from "./EventItem";
+
+interface Props {
+  items;
+}
+
+const EventList: FC<Props> = ({ items }) => {
+  return (
+    <ul>
+      {items.map((event) => (
+        <EventItem key={event.id} id={event.id} title={event.title} location={event.location} date={event.date} image={event.image} />
+      ))}
+    </ul>
+  );
+};
+
+export default EventList;
