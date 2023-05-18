886268e98e88c831984d68096c2e68a573f432ff
alfonsojim1897 <alfonsojim1897@gmail.com>
2023-05-18 22:50:52 +0200

    Use comps from new folder into file

--- pages/events/[eventId].tsx
+++ pages/events/[eventId].tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import EventItem from "../../components/events/EventItem";
+import { getEventById } from "../../dummy-data";
+import { useRouter } from "next/router";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
 const SpecificEventPage = () => {
-  return <div>Event Details Page</div>;
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <React.Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title} />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </React.Fragment>
+  );
 };
 
 export default SpecificEventPage;
