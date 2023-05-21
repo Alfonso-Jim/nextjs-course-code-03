a8f50723512cbdcf4579e4288466ec2158c97f1a
alfonsojim1897 <alfonsojim1897@gmail.com>
2023-05-21 23:30:54 +0200

    Use new comp in file

--- pages/events/[eventId].tsx
+++ pages/events/[eventId].tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/router";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/ErrorAlert";
 
 const SpecificEventPage = () => {
   const router = useRouter();
@@ -12,7 +13,11 @@ const SpecificEventPage = () => {
   const event = getEventById(eventId);
 
   if (!event) {
-    return <p>No event found!</p>;
+    return (
+      <ErrorAlert>
+        <p>No event found!</p>
+      </ErrorAlert>
+    );
   }
 
   return (
