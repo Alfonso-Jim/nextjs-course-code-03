e74ada4bbe1f0c2063b3d19bd29c5e3e21e41641
alfonsojim1897 <alfonsojim1897@gmail.com>
2023-05-21 22:03:07 +0200

    Add new comp into index file

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,12 +1,14 @@
 import EventList from "../../components/events/EventList";
+import EventsSearch from "../../components/events/EventsSearch";
 import { getAllEvents } from "../../dummy-data";
 
 const AllEventsPage = () => {
   const allEvents = getAllEvents();
   return (
-    <div>
+    <>
+      <EventsSearch />
       <EventList items={allEvents} />
-    </div>
+    </>
   );
 };
 
