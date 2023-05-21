ffd1b149947eb8960059e90f347d405212f5b978
alfonsojim1897 <alfonsojim1897@gmail.com>
2023-05-21 23:31:17 +0200

    Use new comp & pass findEventsHandler

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,12 +1,18 @@
 import EventList from "../../components/events/EventList";
 import EventsSearch from "../../components/events/EventsSearch";
+import { useRouter } from "next/router";
 import { getAllEvents } from "../../dummy-data";
 
 const AllEventsPage = () => {
   const allEvents = getAllEvents();
+  const router = useRouter();
+  const findEventsHandler = (year: string, month: string) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={allEvents} />
     </>
   );
