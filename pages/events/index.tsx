68dad9fad0b6c63b6b40496213a067f32c26469a
alfonsojim1897 <alfonsojim1897@gmail.com>
2023-07-10 17:57:26 +0200

    Refactoring all events page

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,11 +1,11 @@
 import EventList from '../../components/events/EventList';
 import EventsSearch from '../../components/events/EventsSearch';
 import { useRouter } from 'next/router';
-import { getAllEvents } from '../../dummy-data';
-import { NextPage } from 'next';
+import { getAllEvents } from '../../helpers/api-util';
+import { GetStaticProps, NextPage } from 'next';
+import { Events } from '../../interfaces/Interfaces';
 
-const AllEventsPage: NextPage = () => {
-  const allEvents = getAllEvents();
+const AllEventsPage: NextPage<Events> = (props) => {
   const router = useRouter();
   const findEventsHandler = (year: string, month: string) => {
     const fullPath = `/events/${year}/${month}`;
@@ -14,9 +14,15 @@ const AllEventsPage: NextPage = () => {
   return (
     <>
       <EventsSearch onSearch={findEventsHandler} />
-      <EventList events={allEvents} />
+      <EventList events={props.events} />
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allEvents = await getAllEvents();
+
+  return { props: { events: allEvents }, revalidate: 5 };
 };
 
 export default AllEventsPage;
