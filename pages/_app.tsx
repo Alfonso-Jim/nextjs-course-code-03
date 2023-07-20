ce8c85bdffc8dff6e60f31e44998b0a2d00e54ba
alfonsojim1897 <alfonsojim1897@gmail.com>
2023-07-20 15:13:29 +0200

    head on _app.js file practice

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
 
-import "../styles/globals.css";
-import Layout from "../components/layout/Layout";
+import '../styles/globals.css';
+import Layout from '../components/layout/Layout';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <Component {...pageProps} />;
     </Layout>
   );
