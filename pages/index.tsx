719e7ecf96f85999c4bde8d8c4dfe1170771a2fe
alfonsojim1897 <alfonsojim1897@gmail.com>
2023-07-20 14:28:18 +0200

    Head & meta practice

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,15 @@ import { GetStaticProps, NextPage } from 'next';
 import EventList from '../components/events/EventList';
 import { Events } from '../interfaces/Interfaces';
 import { getFeaturedEvents } from '../helpers/api-util';
+import Head from 'next/head';
 
 const HomePage: NextPage<Events> = (props) => {
   return (
     <div>
+      <Head>
+        <title>Next Events</title>
+        <meta name='description' content='Find a lot off cool events that allow you to inform yourself my dudes' />
+      </Head>
       <EventList events={props.events} />
     </div>
   );
